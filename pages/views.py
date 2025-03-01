2a5fe04c86a08de064398f0eb0e1cadf9df41690
yousefm <yousefm@umich.edu>
2025-02-28 23:40:21 -0500

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page_view(request):
+    return HttpResponse("Homepage")
+
+def about_page_view(request):
+    context = {
+        "name": "Yousef",
+        "age": 21,  # new
+    }
+    return render(request, "pages/about.html", context)
